4b9c4280676098abb093a52950f623d2c988c32c
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-17 22:10:18 +0530

    Add user verify as a midleware

---
+++ src/midleware/autho.midleware.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/apiError.js"
+import { ApiResponse } from "../utils/apiResponse.js"
+import { User } from "../models/user.model.js"
+
+
+
+export const verifyUserJwt = asyncHandler(async (req, res, next)  =>{
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
+    
+        if (!token) {
+            throw new ApiError(401, "unathorized token")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")   
+    
+        if (!user) {
+            throw new ApiError(409, "invalid access token")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "invalid access token")
+    }
+})
