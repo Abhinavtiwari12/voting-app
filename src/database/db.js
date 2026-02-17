4752885d859c41c56ed4e67d1ddde3bf50c7fa40
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-17 16:57:29 +0530

    Add database and there dependencies

---
+++ src/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectedDB = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n mongodb connected successfully !! DB Host: ${connectedDB.connection.host} `)
+    } catch (error) {
+        console.log("MongoDB connection failed", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
