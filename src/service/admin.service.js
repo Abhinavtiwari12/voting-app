ef86e06e9d890b3e0f837041768cf9be2333c2fb
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-18 22:35:58 +0530

    create admin service

---
+++ src/service/admin.service.js
@@ -0,0 +1,39 @@
+import { Candidate } from "../models/candidate.model.js";
+
+
+export const findCandidate = async (condition) => {
+  const candidate = await Candidate.findOne(condition);
+
+  if (!candidate) {
+    return {
+      success: false,
+      message: "No candidate exist with this detail",
+      data: null,
+    };
+  }
+
+  return {
+    success: true,
+    message: "candidate already exists",
+    data: candidate,
+  };
+};
+
+export const registerCandidate = async (createUser) => {
+  const candidate = await Candidate.create(createUser);
+
+  if (!candidate) {
+    return {
+      success: false,
+      message: "candidate not created",
+      data: null,
+    };
+  }
+
+  return {
+    success: true,
+    message: "candidate created successfully",
+    data: candidate,
+  };
+};
+
