0e4c6c1554d419e9b0017e3d4f46702ea4af0e86
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-19 11:24:50 +0530

    add candidate controller and add register New Candidate, update Candidate, delete Candidate APIs

---
+++ src/controllers/candidate.controller.js
@@ -0,0 +1,91 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/apiError.js"
+import { ApiResponse } from "../utils/apiResponse.js"
+import { Candidate } from "../models/candidate.model.js"
+import { findCandidate, registerCandidate } from "../service/admin.service.js"
+
+
+const registerNewCandidate = asyncHandler( async (req, res) => {
+
+    const {fullname, age, party, addharNumber, candidateId} = req.body
+
+    if(
+        [fullname, age, party, addharNumber, candidateId].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All field are require.")
+    }
+
+    const checkExistingUserCond = {
+        addharNumber
+    }
+    const existedUser = await findCandidate(checkExistingUserCond)
+
+    if (existedUser.success) {
+        throw new ApiError(409, existedUser.message)
+    }
+
+    const createUser = {
+        fullname,
+        age,
+        addharNumber,
+        party,
+        candidateId
+
+    }
+
+    const registeredCandidate = await registerCandidate(createUser)
+
+    return res.status(201).json(
+        new ApiResponse(201, registeredCandidate.data, registeredCandidate.message)
+    )
+
+})
+
+
+const updateCandidate = asyncHandler( async (req, res) => {
+    
+
+    const {fullname, age, addharNumber, party, candidateId} = req.body
+
+    const candidate = await Candidate.findOneAndUpdate(
+ 
+        { candidateId: candidateId},
+        {
+            $set: {
+                fullname,
+                age,
+                addharNumber,
+                party
+            }
+        },
+        {new: true}
+    )
+
+    if (!candidate) {
+        throw new ApiError(400, "somthing went wrong please check candidate id.")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, candidate, "Account updated successfully"))
+})
+
+
+const deleteCandidate = asyncHandler(async (req, res) => {
+    const {candidateId} = req.body
+
+    const candidate = await Candidate.findOneAndDelete(
+        {candidateId: candidateId}
+    )
+
+    if (!candidate) {
+        throw new ApiError(404, "candidate does not dound")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, product, "candidate delete sucessfull.")
+    )
+})
+
+
+export { registerNewCandidate, updateCandidate, deleteCandidate }
