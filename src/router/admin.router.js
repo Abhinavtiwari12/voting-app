3da557b5a58673fe6f12d56d1c965671870c4a30
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-20 15:23:39 +0530

    add router for admin

--- src/router/admin.router.js
+++ src/router/admin.router.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { registerNewCandidate, updateCandidate, deleteCandidate  } from "../controllers/candidate.controller.js";
 import { verifyAdminJwt } from "../midleware/autho.midleware.js";
+import { adminlogin, adminlogout, adminProfile, registerNewAdmin } from "../controllers/admin.conrtoller.js";
 
 
 
 const router = Router()
 
-
+router.route('/registerNewAdmin').post(registerNewAdmin)
+router.route('/adminlogin').post(adminlogin)
+router.route('/adminProfile').post(verifyAdminJwt, adminProfile)
+router.route('/adminlogout').post(verifyAdminJwt, adminlogout)
 router.route('/registerNewCandidate').post(verifyAdminJwt , registerNewCandidate)
 router.route('/updatecandidate').put(verifyAdminJwt, updateCandidate)
 router.route('/deletecandidate').delete(verifyAdminJwt, deleteCandidate)
