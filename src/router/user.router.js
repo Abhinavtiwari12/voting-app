e31e6592421af5206868f360aec58ce38832a1b6
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-21 22:28:29 +0530

    fix error and add router

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { registerNewUser, userlogin, userProfile, changeUserPassword, userlogout  } from "../controller/user.controller.js";
+import { 
+    registerNewUser, 
+    userlogin, 
+    userProfile, 
+    changeUserPassword, 
+    userlogout, 
+    voteCandidate  
+} from "../controllers/user.controller.js";
 import { verifyUserJwt } from "../midleware/autho.midleware.js";
 
 
@@ -10,8 +17,9 @@ const router = Router()
 router.route('/registerUser').post(registerNewUser)
 router.route('/userlogin').post(userlogin)
 router.route('/userlogout').post(verifyUserJwt, userlogout)
-router.route('/userlogin/changeUserPassword').post(verifyUserJwt, changeUserPassword)
+router.route('/changeUserPassword').post(verifyUserJwt, changeUserPassword)
 router.route("/userProfile").post(verifyUserJwt, userProfile)
+router.route('/voteCandidate').post(verifyUserJwt, voteCandidate)
 
 
 // export default router
