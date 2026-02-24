581cd15c707c3566c6cc4e09fe3bdfa1a74fc68e
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-24 11:51:41 +0530

    make router for get Winner API for admin

--- src/router/admin.router.js
+++ src/router/admin.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { registerNewCandidate, updateCandidate, deleteCandidate  } from "../controllers/candidate.controller.js";
-import { adminlogin, adminlogout, adminProfile, registerNewAdmin } from "../controllers/admin.conrtoller.js";
+import { adminlogin, adminlogout, adminProfile, getWinner, registerNewAdmin } from "../controllers/admin.conrtoller.js";
 import { verifyAdminJwt } from "../midleware/autho.midleware.js";
 
 
@@ -14,6 +14,7 @@ router.route('/adminlogout').post(verifyAdminJwt, adminlogout)
 router.route('/registerNewCandidate').post(verifyAdminJwt , registerNewCandidate)
 router.route('/updatecandidate').put(verifyAdminJwt, updateCandidate)
 router.route('/deletecandidate').delete(verifyAdminJwt, deleteCandidate)
+router.route('/getWinner').get(verifyAdminJwt, getWinner)
 
 
 
